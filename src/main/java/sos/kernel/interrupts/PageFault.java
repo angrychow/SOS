e494c32d0152cd5ec0c7c53871d9914e825fa619
angrychow <angrychow@qq.com>
2024-05-17 18:07:38 +0800

    finished

--- src/main/java/sos/kernel/interrupts/PageFault.java
+++ src/main/java/sos/kernel/interrupts/PageFault.java
@@ -13,6 +13,8 @@ public class PageFault {
     public static void PageFaultServices(PCB relativeProcess, int cputick) throws Exception {
         relativeProcess.IntPageFault = false;
         controller.PageReenter(relativeProcess, relativeProcess.IntVirAddr, cputick);
+        System.out.println("[Page ReEnter]");
+        System.out.println(relativeProcess.IntVirAddr);
         relativeProcess.ProcessState = PCB.State.READY;
     }
 }
